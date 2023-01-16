7f4344e72eb21e4b6fc4cdf866c320492e514214
Qumber Rizvi <qumberrizvi@ymail.com>
2023-01-16 06:46:29 +0530

    routes

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
