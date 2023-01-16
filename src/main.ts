15c440ac27f7fe1150af69cb58eaada80a2d4e57
Qumber Rizvi <qumberrizvi@ymail.com>
2023-01-16 06:38:17 +0530

    routes

--- src/main.ts
+++ src/main.ts
@@ -15,11 +15,12 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
+  SwaggerModule.setup('', app, document, {
     customSiteTitle: 'Contacts APIs',
     customCss: '.swagger-ui .topbar { display: none }',
   });
 
   await app.listen(3000);
 }
+
 bootstrap();
